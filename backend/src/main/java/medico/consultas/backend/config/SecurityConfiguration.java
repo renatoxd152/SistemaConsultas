7d8cde0d3c7e248a7b8048411abb9d7cd78b0868
renatoxd152 <renatomorillo@gmail.com>
2025-04-14 16:33:29 -0300

    feat: Adicionado politica de segurança no backend

--- backend/src/main/java/medico/consultas/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/medico/consultas/backend/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -70,6 +71,7 @@ public class SecurityConfiguration {
                 .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_REQUIRED).authenticated()
                 .requestMatchers(ENDPOINTS_ADMIN).hasRole("ADMINISTRATOR")
                 .requestMatchers(ENDPOINTS_CUSTOMER).hasRole("CUSTOMER")
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest().denyAll()).addFilterBefore(userAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
